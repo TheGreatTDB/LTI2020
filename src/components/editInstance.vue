e5b415eda70a831730145905370027e42450ff27
0xGUI <silva.guilherme@protonmail.ch>
2020-04-08 22:34:15 +0100

    Create Edit Instance

--- src/components/editInstance.vue
+++ src/components/editInstance.vue
@@ -2,66 +2,9 @@
   <div>
     <br />
     <p>Instance Name:</p>
-    <input v-model="currentInstance.name" placeholder="Instance Name" class="table table-striped" />
-    
-    <div class="table table-striped">
-      <p>Select Image:</p>
-      <multiselect
-        v-model="currentInstance.image"
-        :options="images"
-        :multiple="false"
-        label="name"
-        track-by="name"
-        placeholder="Pick 1 Image"
-        class="table table-striped"
-      >
-        <template slot="selection" slot-scope="{ values, search, isOpen }">
-          <span
-            class="multiselect__single"
-            v-if="values.length &amp;&amp; !isOpen"
-          >{{ values.length }} options selected</span>
-        </template>
-      </multiselect>
-
-      <p>Select Flavor:</p>
-      <multiselect
-        v-model="currentInstance.flavor"
-        :options="flavors"
-        :multiple="false"
-        label="name"
-        track-by="name"
-        placeholder="Pick 1 Flavor"
-        class="table table-striped"
-      >
-        <template slot="selection" slot-scope="{ values, search, isOpen }">
-          <span
-            class="multiselect__single"
-            v-if="values.length &amp;&amp; !isOpen"
-          >{{ values.length }} options selected</span>
-        </template>
-      </multiselect>
-
-    <!--
-      <p>Select(s) Network(s):</p>
-      <multiselect
-        v-model="instanceNetworks"
-        :options="networks"
-        :multiple="true"
-        label="name"
-        track-by="name"
-        placeholder="Pick 1 or more Networks"
-        class="table table-striped"
-      >
-        <template slot="selection" slot-scope="{ values, search, isOpen }">
-          <span
-            class="multiselect__single"
-            v-if="values.length &amp;&amp; !isOpen"
-          >{{ values.length }} options selected</span>
-        </template>
-      </multiselect>
-      -->
-    </div>
-    <b-button v-on:click.prevent="createInstance()">Create Instance</b-button>
+    <b-form-input class="w-25 mx-auto" v-model="currentInstance.name" placeholder="Instance Name" />
+    <br/>
+    <b-button  variant="outline-primary"  v-on:click.prevent="createInstance()">Create Instance</b-button>
   </div>
 </template>
 <script>
@@ -72,6 +15,7 @@ export default {
       instanceName: null,
       instanceImageRef: null,
       instanceFlavorRef: null,
+      description: null
       //instanceNetworks: null,
       //instanceNetworksUuid: []
     };
@@ -84,10 +28,12 @@ export default {
         }
       });
 
-
       axiosEditInstance
         .put("/compute/v2.1/servers/" + this.currentInstance.id, {
-          server: this.currentInstance
+          server: {
+            name: this.currentInstance.name
+            // "description": "sdfasfsfa"
+          }
         })
         .then(response => {
           console.log(response);
@@ -97,18 +43,16 @@ export default {
           console.log("Failed to create Instance");
           console.log(error);
         });
-    },
+    }
   },
-  created(){
-    console.log("INSIDE EDIT INSTANCE")
-    console.log(this.currentInstance)
+  created() {
+    console.log("INSIDE EDIT INSTANCE");
+    console.log(this.currentInstance);
 
     this.instanceName = this.currentInstance.name;
     this.instanceImageRef = this.currentInstance.image;
     this.instanceNetworks = this.currentInstance.networks;
     this.instanceFlavorRef = this.currentInstance.flavor;
-
-
   }
 };
 </script>
